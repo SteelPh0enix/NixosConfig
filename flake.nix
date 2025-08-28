e49db84bc265b5c517f3628efa796d7d558f4950
SteelPh0enix <wojciech_olech@hotmail.com>
2025-08-28 21:22:38 +0200

    Config template

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "My NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      ...
+    }@inputs:
+    let
+      inherit (self) outputs;
+    in
+    {
+      nixosConfigurations = {
+        steelph0enix-pc =
+          let
+            username = "steelph0enix";
+            specialArgs = { inherit username inputs outputs; };
+          in
+          nixpkgs.lib.nixosSystem {
+            inherit specialArgs;
+            system = "x86_64-linux";
+            modules = [
+              ./nixos/configuration.nix
+
+              home-manager.nixosModules.home-manager
+              {
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+                home-manager.extraSpecialArgs = inputs // specialArgs;
+                home-manager.users.${username} = import ./home-manager/home.nix;
+              }
+            ];
+          };
+      };
+    };
+}
