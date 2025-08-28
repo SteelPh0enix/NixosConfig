9f36896954b193e4af115ad2a6788b1fe099bc1c
SteelPh0enix <wojciech_olech@hotmail.com>
2025-08-28 21:39:30 +0200

    added network.nix

--- nixos/users.nix
+++ nixos/users.nix
@@ -15,5 +15,8 @@
       "networkmanager"
       "render"
     ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPoBzFN664gkpNbjnvTkMaO2zlI0rQTto1gJ+B26fbvO phoen@SteelPh0enixROG"
+    ];
   };
 }
