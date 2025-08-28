ed9cc478d865ebdc1c5627faec5f354cdb0a74a3
SteelPh0enix <wojciech_olech@hotmail.com>
2025-08-28 21:35:46 +0200

    added locale.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -4,10 +4,12 @@
 }:
 {
   system.stateVersion = "25.05";
+  nixpkgs.config.allowUnfree = true;
 
   imports = [
     ./hardware-configuration.nix
     ./boot.nix
     ./users.nix
+    ./locale.nix
   ];
 }
