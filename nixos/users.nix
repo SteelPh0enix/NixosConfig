81234927ae97890b1220b6d6ca2afbbcd86b36ca
SteelPh0enix <wojciech_olech@hotmail.com>
2025-08-28 22:54:54 +0200

    restored openssh key for user

--- nixos/users.nix
+++ nixos/users.nix
@@ -19,5 +19,8 @@
       "pcap"
       "wireshark"
     ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPoBzFN664gkpNbjnvTkMaO2zlI0rQTto1gJ+B26fbvO phoen@SteelPh0enixROG"
+    ];
   };
 }
