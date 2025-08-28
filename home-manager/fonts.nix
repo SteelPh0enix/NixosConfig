04d594e66eaa1cda3cb26154a93e5139941598da
SteelPh0enix <wojciech_olech@hotmail.com>
2025-08-28 23:13:35 +0200

    moved user font settings to home-manager

---
+++ home-manager/fonts.nix
@@ -0,0 +1,14 @@
+{ ... }:
+{
+  fonts = {
+    fontconfig = {
+      defaultFonts = {
+        serif = [ "Noto Serif" ];
+        emoji = [ "Noto Color Emoji" ];
+        sansSerif = [ "Noto Sans" ];
+        monospace = [ "Monaspace Krypton NF" ];
+      };
+      enable = true;
+    };
+  };
+}
