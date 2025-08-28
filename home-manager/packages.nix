2ed2caf331879146354c3c57f1e9544fb47fd04f
SteelPh0enix <wojciech_olech@hotmail.com>
2025-08-28 23:28:33 +0200

    added virtualisation.nix

--- home-manager/packages.nix
+++ home-manager/packages.nix
@@ -5,7 +5,24 @@
     keepassxc
     protonvpn-gui
     teams-for-linux
-    vscode
     wezterm
   ];
+
+  programs.uv.enable = true;
+  programs.vscode.enable = true;
+  programs.wezterm.enable = true;
+  programs.wezterm.extraConfig = ''
+    local wezterm = require('wezterm')
+    local config = wezterm.config_builder()
+
+    config.color_scheme = 'Kanagawa (Gogh)'
+    config.font_size = 10.5
+    config.font = wezterm.font 'MonaspiceKr NF'
+    config.initial_cols = 120
+    config.initial_rows = 30
+
+    return config'';
+  programs.yt-dlp.enable = true;
+
+  qt.enable = true;
 }
