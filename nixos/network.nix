9de5c25e1292071fb6568697c2a2a4d897c67875
SteelPh0enix <wojciech_olech@hotmail.com>
2025-08-28 22:48:35 +0200

    added open ports to networking

--- nixos/network.nix
+++ nixos/network.nix
@@ -2,4 +2,10 @@
 {
   networking.networkmanager.enable = true;
   networking.hostName = "steelph0enix-pc";
+
+  networking.firewall.allowedTCPPorts = [
+    80
+    443
+  ];
+  networking.firewall.allowedUDPPorts = [ ];
 }
