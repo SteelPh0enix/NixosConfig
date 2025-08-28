921e5f586b04af8f10f90627f4c4c176d15788a7
SteelPh0enix <wojciech_olech@hotmail.com>
2025-08-28 23:01:37 +0200

    minor tweaks

---
+++ nixos/hardware.nix
@@ -0,0 +1,24 @@
+{ ... }:
+{
+  hardware.graphics.enable = true;
+  hardware.amdgpu = {
+    amdvlk.enable = true;
+    amdvlk.support32Bit.enable = true;
+    opencl.enable = true;
+  };
+
+  # force RADV
+  environment.variables.AMD_VULKAN_ICD = "RADV";
+  environment.variables.VK_ICD_FILENAMES = "/run/opengl-driver/share/vulkan/icd.d/radeon_icd.x86_64.json";
+
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+    media-session.enable = true;
+  };
+}
