c4a9d7a1b23eac6c20b60566cb4a84dc02fc03ac
SteelPh0enix <wojciech_olech@hotmail.com>
2025-08-29 09:33:59 +0200

    Added nixos-unstable to packages

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,10 +1,15 @@
 {
   pkgs,
+  nixpkgs,
+  nixpkgs-unstable,
   ...
 }:
 {
   system.stateVersion = "25.05";
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
 
   imports = [
     ./hardware-configuration.nix
