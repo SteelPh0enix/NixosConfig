0fbab956cc09977fb0904242fc58303fc4de16ca
SteelPh0enix <wojciech_olech@hotmail.com>
2025-08-30 16:45:34 +0200

    added direnv

--- nixos/shell.nix
+++ nixos/shell.nix
@@ -6,7 +6,7 @@
     lg = "lazygit";
     e = "$EDITOR";
     eh = "$EDITOR .";
-    pkgu = "sudo nixos-rebuild switch --flake .#steelph0enix-pc --upgrade-all";
+    pkgu = "sudo nix-channel --update && sudo nixos-rebuild switch --flake .#steelph0enix-pc --upgrade-all";
     pkgc = "sudo nix-store --gc && sudo nix-store --optimise && sudo nix-collect-garbage -d && nix-collect-garbage -d";
   };
 
