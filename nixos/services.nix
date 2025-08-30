e4397f0363975bfd82c2915a384e42969edd5478
SteelPh0enix <wojciech_olech@hotmail.com>
2025-08-30 17:29:03 +0200

    added fail2ban and fixed ssh config

--- nixos/services.nix
+++ nixos/services.nix
@@ -2,21 +2,22 @@
   networking.networkmanager.enable = true;
   networking.hostName = "steelph0enix-pc";
 
-  networking.firewall.allowedTCPPorts = [
-    80
-    443
-  ];
+  networking.firewall.allowedTCPPorts = [ ];
   networking.firewall.allowedUDPPorts = [ ];
 
   services.openssh = {
     enable = true;
+    ports = [ 33445 ];
+    openFirewall = true;
     settings = {
       X11Forwarding = false;
       PermitRootLogin = "no";
       PasswordAuthentication = false;
+      AllowUsers = [ "steelph0enix" ];
+      LogLevel = "VERBOSE";
     };
-    openFirewall = true;
   };
 
+  services.fail2ban.enable = true;
   services.printing.enable = true;
 }
