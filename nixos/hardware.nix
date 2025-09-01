b3461da30438dc76b6f2c1c11441a5c5da89b3b4
SteelPh0enix <wojciech_olech@hotmail.com>
2025-09-01 11:03:46 +0200

    added explicit amdgpu support

--- nixos/hardware.nix
+++ nixos/hardware.nix
@@ -8,6 +8,12 @@
     amdvlk.support32Bit.enable = true;
     opencl.enable = true;
   };
+  hardware.graphics.extraPackages = with pkgs; [
+    amdvlk
+  ];
+  hardware.graphics.extraPackages32 = with pkgs; [
+    driversi686Linux.amdvlk
+  ];
 
   hardware.bluetooth = {
     enable = true;
