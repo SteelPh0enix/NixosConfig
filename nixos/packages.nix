593e75eb7827959cc9ff71d63cb8e60c74147775
SteelPh0enix <wojciech_olech@hotmail.com>
2025-09-02 18:42:06 +0200

    enabled ccache for llama.cpp

--- nixos/packages.nix
+++ nixos/packages.nix
@@ -38,7 +38,7 @@ let
       (self: super: {
         llama-cpp = llamaPkgs.llamaPackages.llama-cpp.override {
           llamaVersion = "1.2.3";
-          stdenv = super.ccacheStdenv;
+          effectiveStdenv = super.ccacheStdenv;
           buildAllCudaFaQuants = true;
           useMpi = true;
         };
