23744fd6524cd9d5abe74c28fe9e8d6f8fa03e1e
SteelPh0enix <wojciech_olech@hotmail.com>
2025-09-02 19:02:19 +0200

    moved overlays to separate files

--- nixos/packages.nix
+++ nixos/packages.nix
@@ -9,45 +9,12 @@ let
   llamaPkgs = import nixpkgs-unstable {
     overlays = [
       (llama-cpp.overlays.default)
-      (self: super: {
-        ccacheWrapper = super.ccacheWrapper.override {
-          extraConfig = ''
-            export CCACHE_COMPRESS=1
-            export CCACHE_DIR="${config.programs.ccache.cacheDir}"
-            export CCACHE_UMASK=007
-            export CCACHE_SLOPPINESS=random_seed
-            if [ ! -d "$CCACHE_DIR" ]; then
-              echo "====="
-              echo "Directory '$CCACHE_DIR' does not exist"
-              echo "Please create it with:"
-              echo "  sudo mkdir -m0770 '$CCACHE_DIR'"
-              echo "  sudo chown root:nixbld '$CCACHE_DIR'"
-              echo "====="
-              exit 1
-            fi
-            if [ ! -w "$CCACHE_DIR" ]; then
-              echo "====="
-              echo "Directory '$CCACHE_DIR' is not accessible for user $(whoami)"
-              echo "Please verify its access permissions"
-              echo "====="
-              exit 1
-            fi
-          '';
-        };
-      })
-      (self: super: {
-        llama-cpp = llamaPkgs.llamaPackages.llama-cpp.override {
-          llamaVersion = "1.2.3";
-          effectiveStdenv = super.ccacheStdenv;
-          buildAllCudaFaQuants = true;
-          useMpi = true;
-        };
-      })
+      (import ./overlays/ccache.nix { config = config; })
+      (import ./overlays/llama-cpp.nix { llamaPkgs = llamaPkgs; })
     ];
     system = pkgs.system;
     config.allowUnfree = true;
     config.rocmSupport = true;
-    config.rocmGpuTargets = "gfx1100";
   };
 in
 {
