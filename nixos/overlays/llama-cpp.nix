9a58e9eff33e282591aa456adbc0875e95cc9940
SteelPh0enix <wojciech_olech@hotmail.com>
2025-09-02 19:51:45 +0200

    disabled ccache for llama.cpp as it's not working

--- nixos/overlays/llama-cpp.nix
+++ nixos/overlays/llama-cpp.nix
@@ -2,7 +2,6 @@
 (self: super: {
   llama-cpp = llamaPkgs.llamaPackages.llama-cpp.override {
     llamaVersion = "1.2.3";
-    effectiveStdenv = super.ccacheStdenv;
     buildAllCudaFaQuants = true;
     useMpi = true;
     rocmGpuTargets = "gfx1100";
