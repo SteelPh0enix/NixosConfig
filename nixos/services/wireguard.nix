4a8d3088a7f202543090128b577cfd4b3e220f2f
SteelPh0enix <wojciech_olech@hotmail.com>
2025-10-01 11:31:09 +0200

    updated wireguard config

--- nixos/services/wireguard.nix
+++ nixos/services/wireguard.nix
@@ -1,34 +1,48 @@
 { pkgs, ... }:
+let
+  internetIface = "enp191s0";
+  wireguardPort = 16969;
+  wireguardIp = "10.69.69.69/24";
+in
 {
-  networking.nat.enable = true;
-  networking.nat.externalInterface = "enp191s0";
-  networking.nat.internalInterfaces = [ "wg0" ];
-  networking.firewall = {
-    allowedUDPPorts = [ 16969 ];
+  networking.nat = {
+    enable = true;
+    enableIPv6 = true;
+    externalInterface = internetIface;
+    internalInterfaces = [ "wg0" ];
   };
 
-  networking.wireguard.enable = true;
-  networking.wireguard.interfaces.wg0 = {
-    ips = [ "10.69.69.1/24" ];
-    listenPort = 16969;
-    # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
-    # For this to work you have to set the dnsserver IP of your router (or dnsserver of choice) in your clients
-    postSetup = ''
-      ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.69.69.0/24 -o enp191s0 -j MASQUERADE
-    '';
+  networking.firewall.allowedUDPPorts = [
+    wireguardPort
+  ];
 
-    # This undoes the above command
-    postShutdown = ''
-      ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.69.69.0/24 -o enp191s0 -j MASQUERADE
-    '';
+  networking.wg-quick.interfaces = {
+    wg0 = {
+      address = [
+        wireguardIp
+      ];
+      listenPort = wireguardPort;
+      privateKeyFile = "/etc/wireguard/wg-private";
 
-    privateKeyFile = "/home/steelph0enix/.ssh/wg-private";
+      postUp = ''
+        ${pkgs.iptables}/bin/iptables -A FORWARD -i wg0 -j ACCEPT
+        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s ${wireguardIp} -o ${internetIface} -j MASQUERADE
+      '';
 
-    peers = [
-      {
-        publicKey = "QirNxLiqQQdWlsg3B354P7RsM1NmJtCgo8NbqBxeciM=";
-        allowedIPs = [ "10.69.69.69/32" ];
-      }
-    ];
+      postDown = ''
+        ${pkgs.iptables}/bin/iptables -D FORWARD -i wg0 -j ACCEPT
+        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s ${wireguardIp} -o ${internetIface} -j MASQUERADE
+      '';
+
+      peers = [
+        {
+          publicKey = "QirNxLiqQQdWlsg3B354P7RsM1NmJtCgo8NbqBxeciM=";
+          presharedKeyFile = "/etc/wireguard/wg-preshared-mainpc";
+          allowedIPs = [
+            "10.69.69.1/32"
+          ];
+        }
+      ];
+    };
   };
 }
