75864063c4da3fb560c833ca9cb49393785d6f58
SteelPh0enix <wojciech_olech@hotmail.com>
2025-10-01 16:36:52 +0200

    services cleanup

--- nixos/services.nix
+++ nixos/services.nix
@@ -8,6 +8,13 @@
   networking.networkmanager.enable = true;
   networking.hostName = "RX-78-FPC";
 
+  services.fail2ban.enable = true;
+  services.printing.enable = true;
+  services.blueman.enable = true;
+  services.jackett.enable = true;
+  services.flaresolverr.enable = true;
+  services.thermald.enable = true;
+
   networking.firewall = {
     enable = true;
     allowPing = true;
@@ -28,20 +35,11 @@
     };
   };
 
-  services.fail2ban.enable = true;
-  services.printing.enable = true;
-  services.blueman.enable = true;
-
   imports = [ inputs.ucodenix.nixosModules.default ];
   services.ucodenix = {
     enable = true;
     cpuModelId = "00B70F00";
   };
-
-  services.jackett.enable = true;
-  services.flaresolverr.enable = true;
-
-  services.thermald.enable = true;
 
   services.gvfs = {
     enable = true;
