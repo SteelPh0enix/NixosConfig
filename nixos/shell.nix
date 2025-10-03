7e9b0ddff630ae013d9d9a98ec0188c4303c845d
SteelPh0enix <wojciech_olech@hotmail.com>
2025-10-03 15:08:05 +0200

    added docker-here-rocm aliases

--- nixos/shell.nix
+++ nixos/shell.nix
@@ -15,6 +15,8 @@
     cfge = "code ~/nixos-config";
     docker-here = "docker run --rm -v $PWD:$PWD -w $PWD -u $(id -u):$(id -g)";
     docker-here-shell = "docker run --rm -it -v $PWD:$PWD -w $PWD -u $(id -u):$(id -g)";
+    docker-here-rocm = "docker run --device /dev/kfd --device /dev/dri --security-opt seccomp=unconfined --rm";
+    docker-here-shell-rocm = "docker run --device /dev/kfd --device /dev/dri --security-opt seccomp=unconfined --rm -it";
   };
 
   environment.sessionVariables = {
