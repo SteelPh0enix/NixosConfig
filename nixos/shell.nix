a1afb30b7d57e4475440a2357947fccd68d07a58
SteelPh0enix <wojciech_olech@hotmail.com>
2025-10-05 14:42:52 +0200

    removed unnecessary llama env vars

--- nixos/shell.nix
+++ nixos/shell.nix
@@ -28,20 +28,16 @@
 
     LLAMA_ARG_HOST = "0.0.0.0";
     LLAMA_ARG_PORT = 51536;
-    LLAMA_ARG_BATCH = 2048;
-    LLAMA_ARG_UBATCH = 2048;
-    LLAMA_ARG_SWA_FULL = 0;
-    LLAMA_ARG_KV_SPLIT = 0;
-    LLAMA_SET_ROWS = 1; # for LLAMA_ARG_KV_SPLIT=false to work
+    # LLAMA_ARG_BATCH = 2048;
+    # LLAMA_ARG_UBATCH = 2048;
     LLAMA_ARG_FLASH_ATTN = "on";
     LLAMA_ARG_MLOCK = 1;
-    LLAMA_ARG_NO_MMAP = 0;
+    LLAMA_ARG_NO_MMAP = 1;
     LLAMA_ARG_N_GPU_LAYERS = 999;
     LLAMA_OFFLINE = 0;
     LLAMA_ARG_ENDPOINT_SLOTS = 1;
     LLAMA_ARG_ENDPOINT_PROPS = 1;
     LLAMA_ARG_CONTEXT_SHIFT = 1;
-    LLAMA_ARG_NO_CONTEXT_SHIFT = 0;
     UV_TORCH_BACKEND = "auto";
 
     GST_PLUGIN_SYSTEM_PATH_1_0 = "${pkgs.gst_all_1.gstreamer.out}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-plugins-base}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-plugins-good}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-plugins-bad}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-plugins-ugly}/lib/gstreamer-1.0";
