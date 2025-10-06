3c968fe5d6564fdb46e710f5fe4f80313694ed2b
SteelPh0enix <wojciech_olech@hotmail.com>
2025-10-06 11:12:26 +0200

    tweaked vscode install
    tweaked vscode install
    
    tweaked vscode install
    
    tweaked vscode install

--- home-manager/packages.nix
+++ home-manager/packages.nix
@@ -19,7 +19,41 @@ in
   programs.vscode = {
     enable = true;
     mutableExtensionsDir = true;
-    package = pkgsUnstable.vscode;
+    package = pkgsUnstable.vscode.fhsWithPackages (
+      ps: with ps; [
+        automake
+        cmake
+        curl
+        eslint
+        gcc15
+        gdb
+        git-lfs
+        gitFull
+        gnumake
+        lldb
+        llvmPackages_21.clang-tools
+        llvmPackages_21.clang-unwrapped
+        llvmPackages_21.openmp
+        lua
+        luajit
+        ninja
+        nixd
+        nixfmt-rfc-style
+        nixpkgs-review
+        nodejs
+        npm-check
+        openssl.dev
+        pkg-config
+        ruff
+        rustup
+        sqlite
+        uv
+        uv-sort
+        wget
+        yarn
+        zlib
+      ]
+    );
   };
 
   programs.wezterm = {
