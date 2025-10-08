8fae0ee91271e661df123d0724363a82e26ebeb0
SteelPh0enix <wojciech_olech@hotmail.com>
2025-10-08 09:02:08 +0200

    added some ports for services

--- nixos/services.nix
+++ nixos/services.nix
@@ -20,13 +20,20 @@
     allowedTCPPorts = [
       53
       443
+      6969
       8123
       11434
+      16969
+      22137
+      26969
+      36969
+      46969
       51536
       51537
       51538
       51539
       55569
+      56969
     ];
     allowedUDPPorts = [ 53 ];
     extraCommands = ''iptables -t raw -A OUTPUT -p udp -m udp --dport 137 -j CT --helper netbios-ns'';
