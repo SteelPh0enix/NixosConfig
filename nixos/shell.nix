c35018e6d4e6bb0a79ac12e4c60e881153ff94c7
SteelPh0enix <wojciech_olech@hotmail.com>
2025-10-10 16:52:24 +0200

    added OPENAI_XXX env vars for llama.cpp

--- nixos/shell.nix
+++ nixos/shell.nix
@@ -23,6 +23,9 @@
     LLAMA_ARG_NO_CONTEXT_SHIFT = 0;
     UV_TORCH_BACKEND = "auto";
 
+    OPENAI_API_KEY="llamacpp";
+    OPENAI_BASE_URL="http://127.0.0.1:51536/v1";
+
     GST_PLUGIN_SYSTEM_PATH_1_0 = "${pkgs.gst_all_1.gstreamer.out}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-plugins-base}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-plugins-good}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-plugins-bad}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-plugins-ugly}/lib/gstreamer-1.0";
   };
 
