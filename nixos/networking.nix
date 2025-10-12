62c9246233e2604819a10a9afc46eadf1aa9cd15
SteelPh0enix <wojciech_olech@hotmail.com>
2025-10-12 12:48:59 +0200

    moved networking config to networking.nix

---
+++ nixos/networking.nix
@@ -0,0 +1,44 @@
+{
+  networking.networkmanager.enable = true;
+  networking.hostName = "RX-78-FPC";
+
+  networking.firewall = {
+    enable = true;
+    allowPing = true;
+    allowedTCPPorts = [
+      53
+      443
+      6969
+      8123
+      11434
+      16969
+      22137
+      26969
+      36969
+      46969
+      51536
+      51537
+      51538
+      51539
+      55569
+      56969
+    ];
+    allowedUDPPorts = [ 53 ];
+    extraCommands = ''iptables -t raw -A OUTPUT -p udp -m udp --dport 137 -j CT --helper netbios-ns'';
+  };
+
+  networking.nameservers = [ "192.168.0.185" ];
+
+  networking.interfaces.enp191s0 = {
+    ipv4.addresses = [
+      {
+        address = "192.168.0.185";
+        prefixLength = 24;
+      }
+      {
+        address = "192.168.0.222";
+        prefixLength = 24;
+      }
+    ];
+  };
+}
