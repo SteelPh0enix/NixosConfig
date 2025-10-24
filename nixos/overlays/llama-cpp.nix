35ffa254ff52bc724cf20f28511ea3d81ada7ac4
SteelPh0enix <wojciech_olech@hotmail.com>
2025-10-24 18:59:32 +0200

    Updated llama.cpp overlay for llama.cpp master

--- nixos/overlays/llama-cpp.nix
+++ nixos/overlays/llama-cpp.nix
@@ -1,9 +1,9 @@
 { overridePkgs, ... }:
 (self: super: {
   llama-cpp = overridePkgs.llamaPackages.llama-cpp.override {
-    llamaVersion = "1.2.3";
+    llamaVersion = "4.2.0";
     useRocm = false;
     useVulkan = true;
-    enableUma = true;
+    useMpi = true;
   };
 })
