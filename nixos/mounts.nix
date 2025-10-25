cf01759ec38190ef7799811834b62aa412fdb472
SteelPh0enix <wojciech_olech@hotmail.com>
2025-10-25 11:59:30 +0200

    Changed smb secrets path

--- nixos/mounts.nix
+++ nixos/mounts.nix
@@ -66,6 +66,6 @@
       let
         automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,user,users";
       in
-      [ "${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=100" ];
+      [ "${automount_opts},credentials=/home/steelph0enix/smb-secrets,uid=1000,gid=100" ];
   };
 }
