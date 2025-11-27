f20ff43cac1dc7080d4fd60a79a633a1efe380f2
SteelPh0enix <wojciech_olech@hotmail.com>
2025-11-27 09:58:31 +0100

    moved os management aliases to proper functions

--- nixos/init.fish
+++ nixos/init.fish
@@ -74,3 +74,51 @@ function remote-cp --description "Compresses, sends, and decompresses a director
     rm -f "$archive_name"
     echo "Done."
 end
+
+function os-rebuild
+    echo (set_color green)"Directory: ~/nixos-config"(set_color normal)
+    cd ~/nixos-config; or return 1
+
+    echo (set_color yellow)"Rebuilding NixOS (boot) for WorkVM..."(set_color normal)
+    # sudo will likely prompt for password here
+    sudo nixos-rebuild boot --flake .#steelph0enix-work-vm --upgrade-all --print-build-logs --show-trace --refresh
+end
+
+function os-update
+    echo (set_color magenta)"=== Starting OS Update Sequence ==="(set_color normal)
+
+    # 1. Update llama.cpp
+    llama-cpp-update; or return 1
+
+    # 2. Update Flake inputs
+    echo (set_color green)"Directory: ~/nixos-config"(set_color normal)
+    cd ~/nixos-config; or return 1
+
+    echo (set_color blue)"Updating flake inputs..."(set_color normal)
+    nix flake update; or return 1
+
+    # 3. Rebuild System
+    os-rebuild; or return 1
+
+    # 4. Commit changes
+    echo (set_color blue)"Committing flake.lock..."(set_color normal)
+    git add flake.lock && git commit -m 'os update'
+
+    echo (set_color magenta)"=== OS Update Complete ==="(set_color normal)
+end
+
+function os-clean
+    echo (set_color yellow)"Running Store Garbage Collection (Root)..."(set_color normal)
+    sudo nix-store --gc; or return 1
+
+    echo (set_color yellow)"Optimizing Nix Store..."(set_color normal)
+    sudo nix-store --optimise; or return 1
+
+    echo (set_color yellow)"Deleting old generations (Root)..."(set_color normal)
+    sudo nix-collect-garbage -d; or return 1
+
+    echo (set_color yellow)"Deleting old generations (User)..."(set_color normal)
+    nix-collect-garbage -d
+    
+    echo (set_color green)"System Clean Complete."(set_color normal)
+end
