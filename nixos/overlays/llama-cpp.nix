ceab9387a6b4a441cab0434259da0851fd5a9ca0
SteelPh0enix <phoenixpl@hotmail.com>
2026-01-19 19:16:45 +0100

    llama.cpp build fixes

--- nixos/overlays/llama-cpp.nix
+++ nixos/overlays/llama-cpp.nix
@@ -12,5 +12,11 @@
         # allowing access to the network to download the models.
         # Requires 'sandbox = relaxed' or 'false' in /etc/nix/nix.conf
         __noChroot = true;
+
+        # Add 'cacert' to the build inputs so SSL certificates are available
+        nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ [ super.cacert ];
+
+        # Tell CMake/Curl where to find the certificates
+        SSL_CERT_FILE = "${super.cacert}/etc/ssl/certs/ca-bundle.crt";
       });
 })
