298340e61e89146e62bdd1e20d1016293f2c9390
SteelPh0enix <phoenixpl@hotmail.com>
2026-01-22 12:49:27 +0100

    Tweaked llama.cpp server settings

--- nixos/shell.nix
+++ nixos/shell.nix
@@ -4,19 +4,6 @@
     EDITOR = "nvim";
     BROWSER = "firefox";
     TERMINAL = "wezterm";
-
-    LLAMA_ARG_HOST = "0.0.0.0";
-    LLAMA_ARG_PORT = 51536;
-    LLAMA_ARG_FLASH_ATTN = "on";
-    LLAMA_ARG_MLOCK = 1;
-    LLAMA_ARG_N_GPU_LAYERS = 999;
-    LLAMA_OFFLINE = 0;
-    LLAMA_ARG_ENDPOINT_SLOTS = 1;
-    LLAMA_ARG_ENDPOINT_PROPS = 1;
-    LLAMA_ARG_CONTEXT_SHIFT = 1;
-    LLAMA_ARG_FIT = "off";
-    UV_TORCH_BACKEND = "auto";
-
     OPENAI_BASE_URL="http://127.0.0.1:51536/v1";
 
     GST_PLUGIN_SYSTEM_PATH_1_0 = "${pkgs.gst_all_1.gstreamer.out}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-plugins-base}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-plugins-good}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-plugins-bad}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-plugins-ugly}/lib/gstreamer-1.0";
