217a91dc9ca38ee6f13c9688beb86eae3e0c177f
SteelPh0enix <phoenixpl@hotmail.com>
2026-02-04 14:07:11 +0100

    Added os-quick-update

--- home-manager/config.fish
+++ home-manager/config.fish
@@ -94,6 +94,29 @@ function os-update
     echo (set_color magenta)"=== OS Update Complete ==="(set_color normal)
 end
 
+function os-quick-update
+    echo (set_color magenta)"=== Starting quick OS Update Sequence ==="(set_color normal)
+
+    # 1. Update llama.cpp
+    llama-cpp-update; or return 1
+
+    # 2. Update Flake inputs
+    echo (set_color green)"Directory: ~/nixos-config"(set_color normal)
+    cd ~/nixos-config; or return 1
+
+    echo (set_color blue)"Updating flake inputs..."(set_color normal)
+    nix flake update; or return 1
+
+    # 3. Rebuild System
+    os-rebuild; or return 1
+
+    # 4. Commit changes
+    echo (set_color blue)"Committing flake.lock..."(set_color normal)
+    git add flake.lock && git commit -m 'os update'
+
+    echo (set_color magenta)"=== OS Update Complete ==="(set_color normal)
+end
+
 function os-clean
     echo (set_color yellow)"Running Store Garbage Collection (Root)..."(set_color normal)
     sudo nix-store --gc; or return 1
