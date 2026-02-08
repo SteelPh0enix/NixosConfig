c4326237e357908329f4db0eb4cb29d8a65d5864
SteelPh0enix <phoenixpl@hotmail.com>
2026-02-08 12:06:25 +0100

    Config cleanup and package updates

--- nixos/overlays/ollama.nix
+++
@@ -1,8 +0,0 @@
-{ overridePkgs, ... }:
-(self: super: {
-  ollama = super.ollama-rocm.override {
-    rocmPackages = overridePkgs.rocmPackages;
-    rocmGpuTargets = [ "gfx1151" ];
-    acceleration = "rocm";
-  };
-})
