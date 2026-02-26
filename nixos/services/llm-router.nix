5c3e0d6f8c7f117cabe7c1159b11ec50185d69cc
SteelPh0enix <phoenixpl@hotmail.com>
2026-02-26 15:11:25 +0100

    Fixed llm-router service invocation

--- nixos/services/llm-router.nix
+++ nixos/services/llm-router.nix
@@ -1,5 +1,26 @@
-{ pkgs, ... }:
+{ pkgs, pkgsUnstable, ... }:
 
+let
+  llm-router-script = pkgs.writeShellScriptBin "llm-router" ''
+    exec ${pkgsUnstable.llama-cpp}/bin/llama-server \
+      --models-dir /home/LLMs/llama-models/ \
+      --models-preset /home/LLMs/llama-models.ini \
+      --mlock \
+      --direct-io \
+      --fit on \
+      --log-colors on \
+      --offline \
+      --warmup \
+      --host 0.0.0.0 \
+      --port 51536 \
+      --webui \
+      --metrics \
+      --props \
+      --slots \
+      --flash-attn on \
+      --gpu-layers all
+  '';
+in
 {
   systemd.services = {
     "llm-router" = {
@@ -9,23 +30,7 @@
       serviceConfig = {
         Type = "simple";
         WorkingDirectory = "/home/steelph0enix/LLMs";
-        ExecStart = "/run/current-system/sw/bin/llama-server \
-          --models-dir /home/LLMs/llama-models/ \
-          --models-preset /home/LLMs/llama-models.ini \
-          --mlock \
-          --direct-io \
-          --fit on \
-          --log-colors on \
-          --offline \
-          --warmup \
-          --host 0.0.0.0 \
-          --port 51536 \
-          --webui \
-          --metrics \
-          --props \
-          --slots \
-          --flash-attn on \
-          --gpu-layers all";
+        ExecStart = "${llm-router-script}/bin/llm-router";
         Restart = "on-failure";
         RestartSec = 10;
         User = "steelph0enix";
