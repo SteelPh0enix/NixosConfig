6f328f0ff59b06121132a9ed1da4c2162d036252
SteelPh0enix <wojciech_olech@hotmail.com>
2026-04-26 15:18:27 +0200

    Minor fixups

--- nixos/shell.nix
+++ nixos/shell.nix
@@ -26,7 +26,13 @@
     OPENAI_API_KEY="llamacpp";
     OPENAI_BASE_URL="http://127.0.0.1:51536/v1";
 
-    GST_PLUGIN_SYSTEM_PATH_1_0 = "${pkgs.gst_all_1.gstreamer.out}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-plugins-base}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-plugins-good}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-plugins-bad}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-plugins-ugly}/lib/gstreamer-1.0";
+    GST_PLUGIN_SYSTEM_PATH_1_0 = pkgs.lib.makeSearchPathOutput "out" "lib/gstreamer-1.0" [
+      pkgs.gst_all_1.gstreamer
+      pkgs.gst_all_1.gst-plugins-base
+      pkgs.gst_all_1.gst-plugins-good
+      pkgs.gst_all_1.gst-plugins-bad
+      pkgs.gst_all_1.gst-plugins-ugly
+    ];
   };
 
   programs.fish.shellInit = builtins.readFile ./init.fish;
