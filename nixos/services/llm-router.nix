3c16daa822896dffd32e4762c620d3930c3ecb51
SteelPh0enix <wojciech_olech@hotmail.com>
2026-05-02 11:18:58 +0200

    back to 1 model

--- nixos/services/llm-router.nix
+++ nixos/services/llm-router.nix
@@ -7,8 +7,8 @@ let
       --models-preset /home/LLMs/llama-models.ini \
       --host 0.0.0.0 \
       --port 51536 \
-      --models-max 2 \
-      --parallel 2 \
+      --models-max 1 \
+      --parallel 1 \
       --webui \
       --metrics \
       --props \
