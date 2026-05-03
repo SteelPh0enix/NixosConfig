5264b81781b8c5c2347f955b9718cbca70a0196d
SteelPh0enix <wojciech_olech@hotmail.com>
2026-05-03 14:16:28 +0200

    Pi agent env vars

--- nixos/shell.nix
+++ nixos/shell.nix
@@ -1,12 +1,13 @@
 { pkgs, ... }:
 {
   environment.sessionVariables = {
-    EDITOR = "nvim";
+    EDITOR = "code";
+    VISUAL = "code";
     BROWSER = "firefox";
     TERMINAL = "wezterm";
 
-    OPENAI_API_KEY="llamacpp";
-    OPENAI_BASE_URL="http://127.0.0.1:51536/v1";
+    OPENAI_API_KEY = "llamacpp";
+    OPENAI_BASE_URL = "http://127.0.0.1:51536/v1";
 
     GST_PLUGIN_SYSTEM_PATH_1_0 = pkgs.lib.makeSearchPathOutput "out" "lib/gstreamer-1.0" [
       pkgs.gst_all_1.gstreamer
@@ -15,6 +16,10 @@
       pkgs.gst_all_1.gst-plugins-bad
       pkgs.gst_all_1.gst-plugins-ugly
     ];
+
+    PI_OFFLINE = "1";
+    PI_SKIP_VERSION_CHECK = "1";
+    PI_TELEMETRY = "0";
   };
 
   programs.fish.shellInit = builtins.readFile ./init.fish;
