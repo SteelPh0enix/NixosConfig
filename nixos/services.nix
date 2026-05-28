13053f447421f955bb7401cd041e2b57b1201d85
SteelPh0enix <wojciech_olech@hotmail.com>
2026-05-28 10:22:21 +0200

    Disabled wireguard (something broke?)

--- nixos/services.nix
+++ nixos/services.nix
@@ -38,33 +38,33 @@
     cpuModelId = "00A20F10";
   };
 
-  networking.wg-quick.interfaces = {
-    wg0 = {
-      address = [ "10.69.69.1/32" ];
-      privateKeyFile = "/home/steelph0enix/.ssh/wg-private";
+  # networking.wg-quick.interfaces = {
+  #   wg0 = {
+  #     address = [ "10.69.69.1/32" ];
+  #     privateKeyFile = "/home/steelph0enix/.ssh/wg-private";
 
-      peers = [
-        {
-          publicKey = "k7UoJ41C6XaXbHAgfuDvA6ti0WQLM3miZfJKtgX7PFA=";
-          presharedKeyFile = "/home/steelph0enix/.ssh/wg-preshared";
-          allowedIPs = [
-            "0.0.0.0/0"
-            "::/0"
-          ];
+  #     peers = [
+  #       {
+  #         publicKey = "k7UoJ41C6XaXbHAgfuDvA6ti0WQLM3miZfJKtgX7PFA=";
+  #         presharedKeyFile = "/home/steelph0enix/.ssh/wg-preshared";
+  #         allowedIPs = [
+  #           "0.0.0.0/0"
+  #           "::/0"
+  #         ];
 
-          endpoint = "192.168.0.185:16969";
+  #         endpoint = "192.168.0.185:16969";
 
-          # Send keepalives every 25 seconds. Important to keep NAT tables alive.
-          persistentKeepalive = 25;
-        }
-      ];
-    };
-  };
+  #         # Send keepalives every 25 seconds. Important to keep NAT tables alive.
+  #         persistentKeepalive = 25;
+  #       }
+  #     ];
+  #   };
+  # };
 
-  networking.defaultGateway = {
-    address = "10.69.69.1";
-    interface = "wg0";
-  };
+  # networking.defaultGateway = {
+  #   address = "10.69.69.1";
+  #   interface = "wg0";
+  # };
 
   services.avahi.enable = true;
   services.system-config-printer.enable = true;
