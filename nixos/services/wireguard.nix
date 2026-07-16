8c597e7971772c9e3075548af2f5f39a55a0ae55
SteelPh0enix <wojciech_olech@hotmail.com>
2026-07-16 15:53:52 +0200

    Killed proton wg iface to expose the machine properly

--- nixos/services/wireguard.nix
+++ nixos/services/wireguard.nix
@@ -1,21 +1,14 @@
-{ pkgs, ... }:
 let
-  protonIface = "wg-proton";
   wireguardIface = "wg-steelph0enix";
-  internetIface = protonIface;
   wireguardPort = 16969;
-  protonPort = 16970;
   wireguardIp = "10.69.69.69";
   wireguardIpMasked = "${wireguardIp}/24";
-  protonIp = "10.2.0.2";
-  protonIpMasked = "${protonIp}/32";
   localIp = "192.168.0.185";
 in
 {
   networking.nat = {
     enable = true;
     enableIPv6 = true;
-    externalInterface = internetIface;
     internalInterfaces = [
       wireguardIface
     ];
@@ -32,11 +25,6 @@ in
     wireguardPort
   ];
 
-  networking.defaultGateway = {
-    address = protonIp;
-    interface = protonIface;
-  };
-
   networking.wg-quick.interfaces = {
     ${wireguardIface} = {
       autostart = true;
@@ -44,16 +32,6 @@ in
       listenPort = wireguardPort;
       privateKeyFile = "/root/wireguard/wg-private";
       dns = [ localIp ];
-
-      postUp = ''
-        ${pkgs.iptables}/bin/iptables -A FORWARD -i ${wireguardIface} -j ACCEPT
-        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s ${wireguardIpMasked} -o ${internetIface} -j MASQUERADE
-      '';
-
-      postDown = ''
-        ${pkgs.iptables}/bin/iptables -D FORWARD -i ${wireguardIface} -j ACCEPT
-        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s ${wireguardIpMasked} -o ${internetIface} -j MASQUERADE
-      '';
 
       peers = [
         {
@@ -90,25 +68,6 @@ in
           allowedIPs = [
             "10.69.69.10/32"
           ];
-        }
-      ];
-    };
-
-    ${protonIface} = {
-      autostart = true;
-      privateKeyFile = "/root/wireguard/wg-proton-private";
-      address = [ protonIpMasked ];
-      listenPort = protonPort;
-      dns = [ localIp ];
-
-      peers = [
-        {
-          publicKey = "SfUu22F4oN8aDNaZ/O7pNvAorDTREV2Xrx8vT1engn4=";
-          allowedIPs = [
-            "0.0.0.0/0"
-            "::/0"
-          ];
-          endpoint = "79.127.186.164:51820";
         }
       ];
     };
