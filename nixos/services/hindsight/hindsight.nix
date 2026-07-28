76c2a9954142dfb7804eec1c5659e7fd9e6919e9
SteelPh0enix <wojciech_olech@hotmail.com>
2026-07-28 15:08:13 +0200

    Added Hindsight to configuration

---
+++ nixos/services/hindsight/hindsight.nix
@@ -0,0 +1,27 @@
+{ pkgs, ... }:
+
+{
+  systemd.services = {
+    "hindsight" = {
+      description = "Hindsight memory service";
+      enable = true;
+      enableStrictShellChecks = true;
+
+      serviceConfig = {
+        WorkingDirectory = "/etc/nixos/nixos/services/hindsight";
+        ExecStart = "${pkgs.docker}/bin/docker compose -f /etc/nixos/nixos/services/hindsight/docker-compose.yml up --build --remove-orphans --yes";
+        ExecStop = "${pkgs.docker}/bin/docker compose -f /etc/nixos/nixos/services/hindsight/docker-compose.yml down";
+      };
+
+      wantedBy = [ "multi-user.target" ];
+      wants = [
+        "pihole.service"
+        "network-online.target"
+      ];
+      after = [
+        "pihole.service"
+        "network-online.target"
+      ];
+    };
+  };
+}
