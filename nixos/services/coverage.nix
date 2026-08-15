13531a44e31445e71c96b16c27d6be70d02a9e09
SteelPh0enix <wojciech_olech@hotmail.com>
2026-08-15 23:11:41 +0200

    os update

--- nixos/services/coverage.nix
+++ nixos/services/coverage.nix
@@ -2,7 +2,7 @@
 # coverage job in SteelPh0enix/llama-server-api).
 #
 # Layout under /srv/coverage:
-#   llama-server-api/<sha>/{html,lcov.info,coverage.json}
+#   llama-server-api/<sha>/{html,lcov.info}
 #   llama-server-api/latest -> llama-server-api/<newest sha>
 #
 # Browse at http://steelph0enix.framework:6970/
@@ -10,7 +10,7 @@
 
 {
   services.nginx.virtualHosts."6970" = {
-    listen = [ { port = 6970; } ];
+    listen = [ { port = 6970; addr = "0.0.0.0"; } ];
     locations."/".extraConfig = ''
       root /srv/coverage;
       index index.html;
