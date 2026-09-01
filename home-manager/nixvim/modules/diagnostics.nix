45f772ab49b29fa8393b912e7744faf6a0090fcb
SteelPh0enix <wojciech_olech@hotmail.com>
2026-09-01 17:34:18 +0200

    Split nixvim config into multiple files

---
+++ home-manager/nixvim/modules/diagnostics.nix
@@ -0,0 +1,12 @@
+{ ... }:
+{
+  # ---- Diagnostics ----
+  diagnostic.settings = {
+    severity_sort = true;
+    update_in_insert = false;
+    float = {
+      border = "rounded";
+      source = true;
+    };
+  };
+}
