d781208eca114fc7205de0ff27a2e54b1c70ddfb
SteelPh0enix <wojciech_olech@hotmail.com>
2026-09-11 10:20:21 +0200

    Added "Exit terminal mode" keybind to nixvim

--- home-manager/nixvim/modules/keymaps.nix
+++ home-manager/nixvim/modules/keymaps.nix
@@ -81,6 +81,13 @@
       options.desc = "Line diagnostics";
     }
 
+    {
+      mode = "t";
+      key = "<Esc>";
+      action = "<C-\\><C-n>";
+      options.desc = "Exit terminal input mode";
+    }
+
     # --- Clear search highlight ---
     {
       key = "<Esc>";
