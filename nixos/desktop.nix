86df7f78abbf5649a9a36523cfb38c833b68a046
SteelPh0enix <wojciech_olech@hotmail.com>
2026-09-16 14:16:06 +0200

    Removed qt.platformTheme

--- nixos/desktop.nix
+++ nixos/desktop.nix
@@ -6,8 +6,6 @@
   services.xserver.displayManager.lightdm.enable = true;
   services.xserver.desktopManager.xfce.enable = true;
 
-  qt.platformTheme = "gtk2";
-
   environment.systemPackages = with pkgs; [
     hardinfo2
     xclip
